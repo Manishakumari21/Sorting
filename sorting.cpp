b647ade7817d663a825c6bda4f75c18d2d1cd580
Manisha Kumari <manishasingh6114@gmail.com>
2024-08-29 11:07:35 +0530

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+   int main(){
+    vector<int> v(5);
+    for(int i=0;i<5;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());// 0(nlogn)
+    for(int i=0;i<5;i++){
+        cout<<v[i]<<" ";
+    }
+   }
